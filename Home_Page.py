0479b6e92815b9711f3d588ab3582a4d82fa5cba
NickByrdCoding <167818030+NickByrdCoding@users.noreply.github.com>
2025-02-13 22:23:38 -0500

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.title("Web Development Lab02")
 st.header("CS 1301 - Spongebob")
 st.write("Welcome to Lab02 for CS1301! You can navigate between pages by using the sidebar to\nthe left! The following pages are:")
-st.write("1. **Spongebob Portfolio**: A portfolio page that showcases my many hobbies and skills")
+st.write("1. **Spongebob's Portfolio**: A portfolio page that showcases my many hobbies and skills")
 st.write("2. **Data About Me**: A page that displays data about me")
